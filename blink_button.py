ea699c1497d58ebca82e14dd0dfc56fdc9ad9320
Aaron Wald <aaronwald@gmail.com>
2019-09-02 12:17:18 -0400

    read single char from button

--- blink_button.py
+++ blink_button.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
+import sys, tty, termios
+
 from time import sleep     # Import the sleep function from the time module
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(8, GPIO.OUT,
+           initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
+
+fd = sys.stdin.fileno()
+old_settings = termios.tcgetattr(fd)
+try:
+    tty.setraw(sys.stdin.fileno())
+    ch = sys.stdin.read(1)
+finally:
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+print(ch)
 
 while True: # Run forever
     GPIO.output(8, GPIO.HIGH) # Turn on
