465668d83de6e2e9a17133ff4e0ac45b313171f1
Aaron Wald <aaronwald@gmail.com>
2019-10-27 09:29:43 -0400

    startup logic

--- box/think.py
+++ box/think.py
@@ -1,4 +1,22 @@
+import time
+
+oready = False
+while not oready:
+    try:
+        odrv0
+    except: NameError:
+        time.sleep(1)
+    else:
+        oready = True
+
 odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+
+time.sleep(10)
+
+odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+
+
 
 from gpiozero import Button
 from time import sleep
@@ -11,9 +29,8 @@ while True:
     if button.is_pressed:
         print("Pressed")
         serScreen.write(b'Candy')
-        sleep(1)
+        odrv0.axis0.controller.vel_setpoint= 5000; time.sleep(2) ; odrv0.axis0.controller.vel_setpoint= 0
     else:
-#        print("Released")
         pass
 
     
