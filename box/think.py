b1324884948fc638c63584a56514f6f8c7638006
root <root@raspberrypi>
2019-10-27 09:37:46 -0400

    latesT

--- box/think.py
+++ box/think.py
@@ -1,25 +1,27 @@
+#!/usr/bin/python3
+
+from __future__ import print_function
+
+import odrive
+from odrive.enums import *
 import time
 
-oready = False
-while not oready:
-    try:
-        odrv0
-    except: NameError:
-        time.sleep(1)
-    else:
-        oready = True
+# Find a connected ODrive (this will block until you connect one)
+print("finding an odrive...")
+my_drive = odrive.find_any()
 
-odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 
-time.sleep(10)
+my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 
-odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-odrv0.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+while my_drive.axis0.current_state != AXIS_STATE_IDLE:
+    time.sleep(0.1)
+
+my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+my_drive.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
 
 
 
 from gpiozero import Button
-from time import sleep
 import serial
 
 button = Button(2)
@@ -29,7 +31,9 @@ while True:
     if button.is_pressed:
         print("Pressed")
         serScreen.write(b'Candy')
-        odrv0.axis0.controller.vel_setpoint= 5000; time.sleep(2) ; odrv0.axis0.controller.vel_setpoint= 0
+        my_drive.axis0.controller.vel_setpoint= 5000
+	time.sleep(2)
+	my_drive.axis0.controller.vel_setpoint= 0
     else:
         pass
 
