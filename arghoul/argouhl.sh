3cb91d3b198574bee1f4b75f11592a3ccd0def5d
Aaron Wald <aaronwald@gmail.com>
2022-09-10 13:59:45 -0400

    latest

---
+++ arghoul/argouhl.sh
@@ -0,0 +1,17 @@
+openscad  -Dletter=\"A\" -o road-a-thin-face.stl thin-face.scad
+openscad  -Dletter=\"R\" -o road-r-thin-face.stl thin-face.scad
+openscad  -Dletter=\"G\" -o road-g-thin-face.stl thin-face.scad
+openscad  -Dletter=\"H\" -o road-h-thin-face.stl thin-face.scad
+openscad  -Dletter=\"O\" -o road-o-thin-face.stl thin-face.scad
+openscad  -Dletter=\"U\" -o road-u-thin-face.stl thin-face.scad
+openscad  -Dletter=\"L\" -o road-l-thin-face.stl thin-face.scad
+openscad  -Dletter=\"D\" -o road-d-thin-face.stl thin-face.scad
+
+openscad  -Dletter=\"A\" -o road-a-thin.stl thin.scad
+openscad  -Dletter=\"R\" -o road-r-thin.stl thin.scad
+openscad  -Dletter=\"G\" -o road-g-thin.stl thin.scad
+openscad  -Dletter=\"H\" -o road-h-thin.stl thin.scad
+openscad  -Dletter=\"O\" -o road-o-thin.stl thin.scad
+openscad  -Dletter=\"U\" -o road-u-thin.stl thin.scad
+openscad  -Dletter=\"L\" -o road-l-thin.stl thin.scad
+openscad  -Dletter=\"D\" -o road-d-thin.stl thin.scad
