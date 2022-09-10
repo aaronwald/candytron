f817bd951061228a0bb10bc9ccf530273fa74927
Aaron Wald <aaronwald@gmail.com>
2022-09-10 14:13:17 -0400

    set output dir

--- arghoul/argouhl.sh
+++ arghoul/argouhl.sh
@@ -1,12 +1,14 @@
 
+mkdir -p output
+
 for letter in A R G H O U L D 
 do
     echo $letter
-    openscad  -Dletter=\"$letter\" -o road-$letter-thin-face.stl thin-face.scad
-    slic3r --load thin-face.ini -o road-$letter-thin-face.gcode road-$letter-thin-face.stl
+    openscad  -Dletter=\"$letter\" -o output/road-$letter-thin-face.stl thin-face.scad
+    slic3r --load thin-face.ini -o output/road-$letter-thin-face.gcode output/road-$letter-thin-face.stl
 
-    openscad  -Dletter=\"$letter\" -o road-$letter-thin.stl thin.scad
-    slic3r --load thin.ini -o road-$letter-thin.gcode road-$letter-thin.stl
+    openscad  -Dletter=\"$letter\" -o output/road-$letter-thin.stl thin.scad
+    slic3r --load thin.ini -o output/road-$letter-thin.gcode output/road-$letter-thin.stl
 done
 
 
