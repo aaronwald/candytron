fb404add95673c350ba35426048d09e9e00c1a99
Aaron Wald <aaronwald@gmail.com>
2023-11-25 19:41:26 -0500

    add queue and conflation

--- TickerBox/Tests/ticker.py
+++ TickerBox/Tests/ticker.py
@@ -1,8 +1,11 @@
 import json
 import websocket
 import requests
+import threading
+import queue
 
-url = 'http://192.168.1.227'
+ticker_url = 'http://192.168.1.227'
+ws_url = 'wss://ws.kraken.com'
 
 
 # Function to handle WebSocket messages
@@ -14,17 +17,15 @@ def on_message(ws, message):
         for x in data[1]:
             price = x[0]
             print(ticker, price)
-            try:
-                response = requests.get(f"{url}/{ticker}_{x[0]}")
-                if response.status_code == 200:
-                    print(response.json())
-            except e:
-                print(e)
+            my_queue.put([ticker, price])
+
+my_queue = queue.Queue()
+ws = websocket.WebSocketApp(ws_url, on_message=on_message)
+
 
 # Function to connect to the Kraken WebSocket API
 def connect_to_websocket():
     # Define the WebSocket URL
-    ws_url = 'wss://ws.kraken.com'
 
     # Define the subscription message for the BTC/USD trading pair
     subscribe_message = {
@@ -36,7 +37,6 @@ def connect_to_websocket():
     }
 
     # Create a WebSocket connection
-    ws = websocket.WebSocketApp(ws_url, on_message=on_message)
 
     # Send the subscription message to the WebSocket
     ws.on_open = lambda ws: ws.send(json.dumps(subscribe_message))
@@ -47,10 +47,31 @@ def connect_to_websocket():
 # Main function to demonstrate the usage
 def main():
     try:
-        # Connect to the Kraken WebSocket API
-        connect_to_websocket()
+        producer_thread = threading.Thread(target=connect_to_websocket)
+        producer_thread.start()
+
+        while True:
+            try:
+                [ticker,price] = my_queue.get(timeout=1)
+
+                updates = {}
+                updates[ticker] = price
+
+                # conflate any pending updates and send last value only
+                while not my_queue.empty():
+                    [ticker,price] = my_queue.get(block=False)
+                    updates[ticker] = price
+
+                for ticker in updates:
+                    print("Send", ticker, updates[ticker])
+                    response = requests.get(f"{ticker_url}/{ticker}_{updates[ticker]}")
+                    if response.status_code == 200:
+                        print(response.json())
+            except queue.Empty:
+                pass
     except KeyboardInterrupt:
         print("Exiting...")
+        ws.close()
 
 if __name__ == "__main__":
     main()
